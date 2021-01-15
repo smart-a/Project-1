6b61537051092ec86ef23b85d5f6e707e234a811
smart-a <olalekan.adesoye@gmail.com>
2021-01-15 09:44:19 +0100

    Javascript code refactoring

--- script.js
+++ script.js
@@ -1,117 +1,83 @@
-let sandwich = new Sandwich();
-let steak = new Steak();
-let cherries = new Cherries();
-let popsicle = new Popsicle();
+let selected = false;
+let qty = 1;
+
+let sandwich = new Items("sandwich");
+let steak = new Items("steak");
+let cherries = new Items("cherries");
+let popsicle = new Items("popsicle");
 
 const sandwichHandler = () => {
-  sandwich = new Sandwich();
-  sandwich.total();
+  selected = document.getElementById("item1").checked;
+  qty = document.getElementById("sandwich_qty").value;
+  sandwich = new Items("sandwich");
+  document.getElementById(
+    "sandwich_total"
+  ).innerHTML = sandwich.total().toLocaleString();
 };
 
 const steakHandler = () => {
-  steak = new Steak();
-  steak.total();
+  selected = document.getElementById("item2").checked;
+  qty = document.getElementById("steak_qty").value;
+  steak = new Items("steak");
+  document.getElementById(
+    "steak_total"
+  ).innerHTML = steak.total().toLocaleString();
 };
 
 const cherriesHandler = () => {
-  cherries = new Cherries();
-  cherries.total();
+  selected = document.getElementById("item3").checked;
+  qty = document.getElementById("cherries_qty").value;
+  cherries = new Items("cherries");
+  document.getElementById(
+    "cherries_total"
+  ).innerHTML = cherries.total().toLocaleString();
 };
 
 const popsicleHandler = () => {
-  popsicle = new Popsicle();
-  popsicle.total();
+  selected = document.getElementById("item4").checked;
+  qty = document.getElementById("popsicle_qty").value;
+  popsicle = new Items("popsicle");
+  document.getElementById(
+    "popsicle_total"
+  ).innerHTML = popsicle.total().toLocaleString();
 };
 
 const totalHandler = () => {
   let total =
     sandwich.total() + steak.total() + cherries.total() + popsicle.total();
-  if (total > 1000) total = total * 0.15;
+  let discount = 0;
+  if (total > 1000) {
+    discount = total * 0.15;
+    total = total - discount;
+  }
   document.getElementById("grand_total").innerHTML = total.toLocaleString();
-  setTimeout(() => {
-    let response = confirm(
-      "Please confirm the total cost of $" +
-        total.toLocaleString() +
-        " for payment"
-    );
-    if (response) {
-      alert("Thank you for your patronage!!!");
-    } else {
-      alert("Application withdraw");
-    }
-  }, 1000);
+  if (total > 0)
+    setTimeout(() => {
+      let response = confirm(
+        "Please confirm the total cost of $" +
+          total.toLocaleString() +
+          " for payment.\nWith discounted amount of $" +
+          discount.toLocaleString()
+      );
+      if (response) {
+        alert("Thank you for your patronage!!!");
+      } else {
+        alert("Application withdraw");
+      }
+    }, 1000);
 };
 
-function Sandwich() {
-  this.selected = document.getElementById("item1").checked;
-  let tag = document.querySelector("input[name=sandwich]:checked").id;
+function Items(tagName) {
+  this.selected = selected;
   let rate = "0";
+  let tag = document.querySelector(`input[name=${tagName}]:checked`).id;
   let amt = document.getElementById(tag).nextElementSibling.firstElementChild;
   if (amt != null) rate = amt.textContent;
   this.rate = parseInt(rate.replace(",", ""));
-  let qty = document.getElementById("sandwich_qty").value;
   this.qty = parseInt(qty);
   this.total = () => {
     let total = 0;
     if (this.selected) total = this.rate * this.qty;
-    document.getElementById(
-      "sandwich_total"
-    ).innerHTML = total.toLocaleString();
-    return total;
-  };
-}
-
-function Steak() {
-  this.selected = document.getElementById("item2").checked;
-  let tag = document.querySelector("input[name=steak]:checked").id;
-  let rate = "0";
-  let amt = document.getElementById(tag).nextElementSibling.firstElementChild;
-  if (amt != null) rate = amt.textContent;
-  this.rate = parseInt(rate.replace(",", ""));
-  let qty = document.getElementById("steak_qty").value;
-  this.qty = parseInt(qty);
-  this.total = () => {
-    let total = 0;
-    if (this.selected) total = this.rate * this.qty;
-    document.getElementById("steak_total").innerHTML = total.toLocaleString();
-    return total;
-  };
-}
-
-function Cherries() {
-  this.selected = document.getElementById("item3").checked;
-  let tag = document.querySelector("input[name=cherries]:checked").id;
-  let rate = "0";
-  let amt = document.getElementById(tag).nextElementSibling.firstElementChild;
-  if (amt != null) rate = amt.textContent;
-  this.rate = parseInt(rate.replace(",", ""));
-  let qty = document.getElementById("cherries_qty").value;
-  this.qty = parseInt(qty);
-  this.total = () => {
-    let total = 0;
-    if (this.selected) total = this.rate * this.qty;
-    document.getElementById(
-      "cherries_total"
-    ).innerHTML = total.toLocaleString();
-    return total;
-  };
-}
-
-function Popsicle() {
-  this.selected = document.getElementById("item4").checked;
-  let tag = document.querySelector("input[name=popsicle]:checked").id;
-  let rate = "0";
-  let amt = document.getElementById(tag).nextElementSibling.firstElementChild;
-  if (amt != null) rate = amt.textContent;
-  this.rate = parseInt(rate.replace(",", ""));
-  let qty = document.getElementById("popsicle_qty").value;
-  this.qty = parseInt(qty);
-  this.total = () => {
-    let total = 0;
-    if (this.selected) total = this.rate * this.qty;
-    document.getElementById(
-      "popsicle_total"
-    ).innerHTML = total.toLocaleString();
     return total;
   };
 }
